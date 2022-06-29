3f49ad1f994354275dfee2e803532045d11d5c37
ccqstark <ccqstark@qq.com>
2022-06-29 23:44:20 +0800

    [+] zk服务注册发现、rpc异常类

--- qrpc-common/src/main/java/com/ccqstark/qrpc/common/utils/PropertiesFileUtil.java
+++ qrpc-common/src/main/java/com/ccqstark/qrpc/common/utils/PropertiesFileUtil.java
@@ -2,7 +2,8 @@ package com.ccqstark.qrpc.common.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
--- qrpc-core/src/main/java/com/ccqstark/qrpc/core/registry/zk/ZkServiceDiscoveryImpl.java
+++ qrpc-core/src/main/java/com/ccqstark/qrpc/core/registry/zk/ZkServiceDiscoveryImpl.java
@@ -1,4 +1,48 @@
 package com.ccqstark.qrpc.core.registry.zk;
 
-public class ZkServiceDiscoveryImpl {
+import com.ccqstark.qrpc.common.enums.RpcErrorMessageEnum;
+import com.ccqstark.qrpc.common.exception.RpcException;
+import com.ccqstark.qrpc.common.extension.ExtensionLoader;
+import com.ccqstark.qrpc.common.utils.CollectUtil;
+import com.ccqstark.qrpc.core.loadbalance.LoadBalance;
+import com.ccqstark.qrpc.core.registry.ServiceDiscovery;
+import com.ccqstark.qrpc.core.registry.zk.util.CuratorUtils;
+import com.ccqstark.qrpc.core.remoting.dto.RpcRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+/**
+ * @author ccqstark
+ * @description zookeeper服务发现实现
+ * @date 2022/6/29 23:41 
+ */
+@Slf4j
+public class ZkServiceDiscoveryImpl implements ServiceDiscovery {
+
+    private final LoadBalance loadBalance;
+
+    public ZkServiceDiscoveryImpl() {
+        this.loadBalance = ExtensionLoader.getExtensionLoader(LoadBalance.class)
+                .getExtension("loadBalance");
+    }
+
+    @Override
+    public InetSocketAddress lookupService(RpcRequest rpcRequest) {
+        String rpcServiceName = rpcRequest.getRpcServiceName();
+        CuratorFramework zkClient = CuratorUtils.getZkClient();
+        List<String> serviceUrlList = CuratorUtils.getChildrenNodes(zkClient, rpcServiceName);
+        if (CollectUtil.isEmpty(serviceUrlList)) {
+            throw new RpcException(RpcErrorMessageEnum.SERVICE_CAN_NOT_BE_FOUND, rpcServiceName);
+        }
+        // 负载均衡
+        String targetServiceUrl = loadBalance.selectServiceAddress(serviceUrlList, rpcRequest);
+        log.info("Successfully found the service address:[{}]", targetServiceUrl);
+        String[] socketAddressArray = targetServiceUrl.split(":");
+        String host = socketAddressArray[0];
+        int post = Integer.parseInt(socketAddressArray[1]);
+        return new InetSocketAddress(host, post);
+    }
 }
