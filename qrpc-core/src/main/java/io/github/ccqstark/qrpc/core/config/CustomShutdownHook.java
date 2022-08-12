e43d6a732a4aa24a60c12dc964563380d3f31097
ccqstark <ccqstark@qq.com>
2022-08-12 12:18:16 +0800

    [*] 补充JVM关闭钩子注释

--- qrpc-core/src/main/java/io/github/ccqstark/qrpc/core/config/CustomShutdownHook.java
+++ qrpc-core/src/main/java/io/github/ccqstark/qrpc/core/config/CustomShutdownHook.java
@@ -9,7 +9,11 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 
-
+/**
+ * @author ccqstark
+ * @description JVM关闭钩子，实现程序的平滑退出（优雅退出）
+ * @date 2022/8/12 12:15
+ */
 @Slf4j
 public class CustomShutdownHook {
 
@@ -21,12 +25,15 @@ public class CustomShutdownHook {
 
     public void clearAll() {
         log.info("add ShutdownHook for clearAll");
+        // 添加JVM关闭钩子
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try {
+                // 移除注册中心对应的服务地址
                 InetSocketAddress inetSocketAddress = new InetSocketAddress(InetAddress.getLocalHost().getHostAddress(), NettyRpcServer.PORT);
                 CuratorUtils.clearRegistry(CuratorUtils.getZkClient(), inetSocketAddress);
             } catch (UnknownHostException ignored) {
             }
+            // 关闭线程池
             ThreadPoolFactoryUtil.shutDownThreadPool();
         }));
     }
